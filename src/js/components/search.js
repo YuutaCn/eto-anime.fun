47ea7e887378cf3153f29979f073f66d1626e572
Yuuta Segawa <yuuta@192.168.100.3>
2022-10-30 23:45:11 +0300

    fix: off search

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -1,30 +1,30 @@
-const minLenght = 3;
-let stringi = '';
+// const minLenght = 3;
+// let stringi = '';
 
-document.querySelector('.header__form-label-input').addEventListener('input', e => {
-  if (document.querySelector('.header__form-label-input').value === '') {
-    document.querySelector('.header__form-search-zone').classList.remove('--active');
-    stringi = '';
-  } else {
-    if (e.data) {
-      stringi += e.data;
-    } else if (e.data == null) {
-      stringi = stringi.slice(0, -1);
-    };
+// document.querySelector('.header__form-label-input').addEventListener('input', e => {
+//   if (document.querySelector('.header__form-label-input').value === '') {
+//     document.querySelector('.header__form-search-zone').classList.remove('--active');
+//     stringi = '';
+//   } else {
+//     if (e.data) {
+//       stringi += e.data;
+//     } else if (e.data == null) {
+//       stringi = stringi.slice(0, -1);
+//     };
 
-    if (stringi.length > 0 && stringi.length < minLenght) {
-      document.querySelector('.header__form-search-zone').classList.add('--active');
-      document.querySelector('.header__form-search-zone').innerHTML = `Минимальная длиннаа запроса ${minLenght} символа`;
-    } else if (stringi.length >= 4) {
-      let searchHtml = '';
-      const searchArray = [...ANIME_PAGE_1].filter(e => e.name.ru.toLowerCase().substr(0, stringi.length) == `${document.querySelector('.header__form-label-input').value.toLowerCase()}`)
-      searchArray.forEach(el => {
-        searchHtml += `
-        <a href="${el.href}">${el.name.ru}</a>
-        `;
-      });
-      console.log(stringi);
-      document.querySelector('.header__form-search-zone').innerHTML = searchHtml;
-    };
-  };
-});
+//     if (stringi.length > 0 && stringi.length < minLenght) {
+//       document.querySelector('.header__form-search-zone').classList.add('--active');
+//       document.querySelector('.header__form-search-zone').innerHTML = `Минимальная длиннаа запроса ${minLenght} символа`;
+//     } else if (stringi.length >= 4) {
+//       let searchHtml = '';
+//       const searchArray = [...ANIME_PAGE_1].filter(e => e.name.ru.toLowerCase().substr(0, stringi.length) == `${document.querySelector('.header__form-label-input').value.toLowerCase()}`)
+//       searchArray.forEach(el => {
+//         searchHtml += `
+//         <a href="${el.href}">${el.name.ru}</a>
+//         `;
+//       });
+//       console.log(stringi);
+//       document.querySelector('.header__form-search-zone').innerHTML = searchHtml;
+//     };
+//   };
+// });
