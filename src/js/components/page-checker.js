198996e36a591df4b25afa57cf8177cffe18dfa1
Yuuta Segawa <yuuta@192.168.100.3>
2022-11-01 13:09:38 +0300

    fix: micro bugs

--- src/js/components/page-checker.js
+++ src/js/components/page-checker.js
@@ -4,3 +4,6 @@ if (window.location.pathname == '/pages/top' || window.location.pathname == '/pa
 if (window.location.pathname == '/pages/2022' || window.location.pathname == '/pages/2022.html') {
   document.querySelectorAll('.header__nav-list-item-href')[1].classList.add('active')
 }
+if (window.location.pathname == '/pages/2022' || window.location.pathname == '/pages/randop.html') {
+  document.querySelectorAll('.header__nav-list-item-href')[2].classList.add('active')
+}
--- src/js/components/select-anime.js
+++ src/js/components/select-anime.js
@@ -8,7 +8,7 @@ class AnimeSelect {
       // };
       return e.genre[0] == genreModificator || e.genre[1] == genreModificator || e.genre[2] == genreModificator || e.genre[3] == genreModificator || e.genre[4] == genreModificator || e.genre[5] == genreModificator || e.genre[6] == genreModificator || e.genre[7] == genreModificator || e.genre[9] == genreModificator || e.genre[8] == genreModificator || e.genre[10] == genreModificator || e.genre[11] == genreModificator || e.genre[12] == genreModificator || e.genre[13] == genreModificator || e.genre[14] == genreModificator || e.genre[15] == genreModificator;
     });
-    // selectResAnime.sort((x, y) => y.score - x.score);
+    selectResAnime.sort((x, y) => y.score - x.score);
     selectResAnime.forEach(({ name, score, cover, href }) => {
       htmlSelectCard += `
       <li class="select-anime__list-item">
